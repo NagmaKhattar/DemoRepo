c7e01f033aae9a4b99ea8d86ef43e4a6550de659
NagmaKhattar <nagmakhattar@gmail.com>
2025-01-17 15:43:50 -0500

    added a comment to show another commit

--- src/friday2025/Friday2025.java
+++ src/friday2025/Friday2025.java
@@ -19,6 +19,7 @@ public class Friday2025 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hi");
+        //showing other commit
     }
     
 }
